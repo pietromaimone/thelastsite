2c6ba4aa9d806dd21a759c9eb3874175db42ceda
pietro maimone <137305920+pietromaimone@users.noreply.github.com>
2025-06-06 18:03:21 -0300

    Update botao.js

--- botao.js
+++ botao.js
@@ -1,1 +1,19 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const banner = document.getElementById('cookie-banner');
+    const acceptBtn = document.getElementById('accept-cookie');
+  
+    if (localStorage.getItem('cookieAccepted')) {
+      banner.style.display = 'none';
+    }
+  
+    acceptBtn.addEventListener('click', () => {
+      localStorage.setItem('cookieAccepted', 'true');
+      banner.classList.add('hide');
+  
+      // Aguarda a transição antes de ocultar
+      setTimeout(() => {
+        banner.style.display = 'none';
+      }, 500); // Tempo da transição
+    });
+  });
+  
